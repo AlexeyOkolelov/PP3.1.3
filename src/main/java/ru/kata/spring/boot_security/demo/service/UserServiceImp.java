d6c94777f4c5cf3e7f6195dac3645bba0caf8fbf
Алексей <okolelov9805@bk.ru>
2022-12-19 20:11:38 +0500

    file

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserRepository;
 
 import ru.kata.spring.boot_security.demo.model.User;
@@ -19,6 +20,7 @@ import java.util.Optional;
 
 
 @Service
+@Transactional
 public class UserServiceImp implements UserService, UserDetailsService {
 
 
